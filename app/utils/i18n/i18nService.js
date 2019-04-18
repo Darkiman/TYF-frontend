c78204a9126110ddfbadaa4bc787d8da4efe9538
Alexey Gavrilov <alex10003@Mail.ru>
2019-04-18 18:41:45 +0300

    saving locale`

---
+++ app/utils/i18n/i18nService.js
@@ -0,0 +1,42 @@
+import i18n from 'i18n-js';
+import AsyncStorage from '@react-native-community/async-storage/types';
+
+import en from './locales/en.json';
+import ru from './locales/ru.json';
+import Languages from "../constants/Languages";
+
+const userLanguageKey = 'userLanguage';
+
+class i18nSingleton {
+    constructor() {
+        i18n.defaultLocale = Languages.EN;
+        i18n.locale = Languages.EN;
+        i18n.fallbacks = true;
+        i18n.translations = { en, ru };
+        this.initialize()
+    }
+
+    async initialize() {
+        const savedLanguage = await AsyncStorage.getItem(userLanguageKey);
+        if(savedLanguage) {
+            i18n.locale = savedLanguage;
+        } else {
+            i18n.locale = 'en';
+            AsyncStorage.setItem(userLanguageKey, Languages.EN);
+        }
+    }
+
+    setLocale(language) {
+        if(i18n.locale !== language) {
+            i18n.locale = language;
+            AsyncStorage.setItem(userLanguageKey, language);
+        }
+    }
+
+    t(text) {
+       return i18n.t(text);
+    }
+}
+
+const i18nService = new i18nSingleton();
+export default i18nService;
