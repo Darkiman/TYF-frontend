dc196ec1b8f0e892706aba62e5fd2114b2904e3c
Alexey Gavrilov <alexey.gavrilov@taskdata.com>
2019-04-24 17:19:43 +0300

    added maps for android

--- app/modules/maps/mapsView.js
+++ app/modules/maps/mapsView.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import {
     View,
@@ -9,21 +10,104 @@ import {
 import ErrorMessage from "../../components/ErrorMessage";
 import {sharedStyles} from "../../shared/sharedStyles";
 
+const LATITUDE_DELTA = 0.01;
+const LONGITUDE_DELTA = 0.01;
+
+const initialRegion = {
+    latitude: -37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+};
+
 const styles = StyleSheet.create({
-    container: {
-        height: 400,
-        width: 400,
+    mapContainer: {
+        position: 'relative',
+        height: '100%'
     },
     map: {
+        ...StyleSheet.absoluteFillObject,
     },
 });
 
 export default class MapsView extends Component {
     constructor(props) {
         super(props);
+
+        this.map = null;
+
+        this.state = {
+            region: {
+                latitude: -37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            },
+            ready: true,
+            filteredMarkers: []
+        };
     }
 
+    setRegion(region) {
+        if(this.state.ready) {
+            setTimeout(() => this.map.animateToRegion(region), 10);
+        }
+    }
+
+    componentDidMount() {
+        this.getCurrentPosition();
+    }
+
+    getCurrentPosition() {
+        try {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const region = {
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude,
+                        latitudeDelta: LATITUDE_DELTA,
+                        longitudeDelta: LONGITUDE_DELTA,
+                    };
+                    this.setRegion(region);
+                },
+                (error) => {
+                    switch (error.code) {
+                        case 1:
+                            if (Platform.OS === "ios") {
+                                alert("", "Para ubicar tu locación habilita permiso para la aplicación en Ajustes - Privacidad - Localización");
+                            } else {
+                                alert("", "Para ubicar tu locación habilita permiso para la aplicación en Ajustes - Apps - ExampleApp - Localización");
+                            }
+                            break;
+                        default:
+                            alert("", "Error al detectar tu locación");
+                    }
+                }
+            );
+        } catch(e) {
+            alert(e.message || "");
+        }
+    };
+
+    onMapReady = (e) => {
+        if(!this.state.ready) {
+            this.setState({ready: true});
+        }
+    };
+
+    onRegionChange = (region) => {
+        console.log('onRegionChange', region);
+    };
+
+    onRegionChangeComplete = (region) => {
+        console.log('onRegionChangeComplete', region);
+    };
+
+
     render() {
+        const { region } = this.state;
+        const { children, renderMarker, markers } = this.props;
+
         const {
             isLoading,
             error,
@@ -31,19 +115,22 @@ export default class MapsView extends Component {
         } = this.props;
         return (
             <SafeAreaView styles={sharedStyles.safeView}>
-                <View>
+                <View style={styles.mapContainer}>
                     {isLoading ? <ActivityIndicator /> : null}
                     {error ? <ErrorMessage/> : null}
                     <MapView
-                        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+                        showsUserLocation
+                        provider={PROVIDER_GOOGLE}
+                        ref={ map => { this.map = map }}
+                        data={markers}
+                        initialRegion={initialRegion}
+                        renderMarker={renderMarker}
+                        onMapReady={this.onMapReady}
+                        showsMyLocationButton={false}
+                        onRegionChange={this.onRegionChange}
+                        onRegionChangeComplete={this.onRegionChangeComplete}
                         style={styles.map}
-                        region={{
-                            latitude: 37.78825,
-                            longitude: -122.4324,
-                            latitudeDelta: 0.015,
-                            longitudeDelta: 0.0121,
-                        }}>
-                    </MapView>
+                    />
                 </View>
             </SafeAreaView>
         );
