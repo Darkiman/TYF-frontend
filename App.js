45d7be501db3adb728692ad3766b393adcd666cc
Alexey Gavrilov <alex10003@Mail.ru>
2019-04-26 14:51:08 +0300

    added loading screen for ios

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
 import React, {Component} from 'react';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import AppNavigation from './app/modules/appNavigation/appNavigation';
+import SplashScreen from 'react-native-splash-screen';
 
 import configureStore from './app/store/configureStore';
 
 const store = configureStore({});
 export default class App extends Component {
-  render() {
-    return (
-        <Provider store={store}>
-            <AppNavigation/>
-        </Provider>
-    )
-  }
+
+    componentDidMount() {
+        SplashScreen.hide();
+    }
+
+    render() {
+        return (
+            <Provider store={store}>
+                <AppNavigation/>
+            </Provider>
+        )
+    }
 }
--- android/app/src/main/java/com/temp/MainApplication.java
+++ android/app/src/main/java/com/temp/MainApplication.java
@@ -3,6 +3,7 @@ package com.temp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SplashScreenReactPackage(),
             new RNDeviceInfo(),
             new AsyncStoragePackage(),
             new VectorIconsPackage(),
