825d0c75dd5e4687d842cbbd7744b0131152d9b6
Alexey Gavrilov <alex10003@Mail.ru>
2019-06-28 16:12:02 +0300

    fixed release api url

--- app/utils/apiConfig.js
+++ app/utils/apiConfig.js
@@ -1,6 +1,6 @@
 import {Platform} from "react-native";
 
-const url = __DEV__ ? (Platform.OS === 'ios' ? 'http://10.0.2.32:3050' : 'http://10.0.2.2:3050') : 'https://tyf-app.com';
+const url = __DEV__ ? (Platform.OS === 'ios' ? 'http://10.0.2.32:3050' : 'http://10.0.2.2:3050') : 'https://api.tyf-app.com';
 
 const apiConfig = {
     // static: Platform.OS === 'ios' ? 'http://127.0.0.1:3050/static/' : 'http://10.0.2.2:3050/static/',
