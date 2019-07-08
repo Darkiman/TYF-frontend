358d8ff29a6b2882db6c997e83ae2ad83f790d9d
Alexey Gavrilov <alex10003@Mail.ru>
2019-07-08 18:45:37 +0300

    added icons to settings menu

--- app/modules/settings/settingsView.js
+++ app/modules/settings/settingsView.js
@@ -1,21 +1,31 @@
 import React, {Component} from 'react';
 import {
   View,
-  Button,
-  Text,
   ActivityIndicator,
   SafeAreaView
 } from 'react-native';
 import NavigationRoutes from "../../constants/NavigationRoutes";
 import ErrorMessage from "../../components/ErrorMessage";
 import {sharedStyles} from "../../shared/styles/sharedStyles";
-import { ListItem } from 'react-native-elements'
+import {Icon, ListItem} from 'react-native-elements'
 import i18nService from '../../utils/i18n/i18nService';
 import userService from "../../utils/userService";
+import IconsType from "../../constants/IconsType";
+import iconsService from "../../utils/iconsService";
+import themeService from "../../utils/themeService";
+
+const colors = themeService.currentThemeColors;
+
+const styles = {
+  iconContainer: {
+      width: 20
+  }
+};
 
 export default class SettingsView extends Component {
     constructor(props) {
         super(props);
+        this.iconPrefix = iconsService.getIconPrefix();
     }
 
     render() {
@@ -30,6 +40,11 @@ export default class SettingsView extends Component {
                     {isLoading ? <ActivityIndicator /> : null}
                     {error ? <ErrorMessage /> : null}
                     <ListItem
+                        leftIcon={<Icon type={IconsType.Ionicon}
+                                        name={`${this.iconPrefix}-globe`}
+                                        containerStyle={styles.iconContainer}
+                                        color={colors.textColor}
+                                        underlayColor={'transparent'}/>}
                         title={i18nService.t('language')}
                         onPress={() => {
                             this.props.navigation.navigate(NavigationRoutes.LANGUAGE_SETTINGS, {
@@ -41,6 +56,11 @@ export default class SettingsView extends Component {
                         }}
                     />
                     <ListItem
+                        leftIcon={<Icon type={IconsType.Ionicon}
+                                        name={`${this.iconPrefix}-lock`}
+                                        containerStyle={styles.iconContainer}
+                                        color={colors.textColor}
+                                        underlayColor={'transparent'}/>}
                         title={i18nService.t('confidentiality')}
                         onPress={() => {
                             this.props.navigation.navigate(NavigationRoutes.CONFIDENTIALITY_SETTINGS, {
@@ -52,12 +72,22 @@ export default class SettingsView extends Component {
                         }}
                     />
                     <ListItem
+                        leftIcon={<Icon type={IconsType.Ionicon}
+                                        name={`${this.iconPrefix}-information-circle`}
+                                        containerStyle={styles.iconContainer}
+                                        color={colors.textColor}
+                                        underlayColor={'transparent'}/>}
                         title={i18nService.t('about_application')}
                         onPress={() => {
                             this.props.navigation.navigate(NavigationRoutes.ABOUT_SETTINGS);
                         }}
                     />
                     <ListItem
+                        leftIcon={<Icon type={IconsType.Ionicon}
+                                        name={`${this.iconPrefix}-exit`}
+                                        containerStyle={styles.iconContainer}
+                                        color={colors.textColor}
+                                        underlayColor={'transparent'}/>}
                         title={i18nService.t('exit')}
                         onPress={async () => {
                             userService.deleteCurrentUser();
